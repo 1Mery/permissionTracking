28b8577f3db864c6c58fe8eaf5a9a0c768de9787
1Mery <meryem.ozevren442001@gmail.com>
2026-01-27 22:00:28 +0300

    first commit

---
+++ src/main/java/com/hospital/permissiontracking/PermissionTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.hospital.permissiontracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermissionTrackingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PermissionTrackingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hospital/permissiontracking/PermissionTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.permissiontracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermissionTrackingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
